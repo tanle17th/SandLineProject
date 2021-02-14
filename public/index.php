ca505e5429ffbb3fa2213b114a3cce8418abb455
Percy <peobayuwana@oasmack.com>
2021-02-13 20:45:07 -0500

    initial setup changes

--- public/index.php
+++ public/index.php
@@ -32,6 +32,7 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 */
 
 require __DIR__.'/../vendor/autoload.php';
+#require __DIR__.'/../SandLineProject/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -45,6 +46,7 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+#$app = require_once __DIR__.'/..SandLineProject/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
